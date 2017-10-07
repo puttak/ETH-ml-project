3949d4a35dd4215520a05538fc7407ee1ba023be
Massimo Bortone <bortonem@student.ethz.ch>
2017-10-07 16:20:43 +0200

    first implementation of feature selection using variance threshold

--- ml_project/models/feature_selection.py
+++ ml_project/models/feature_selection.py
@@ -2,6 +2,7 @@ from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.utils import check_random_state
 from sklearn.utils.validation import check_array, check_is_fitted
 from sklearn.utils.random import sample_without_replacement
+from sklearn.feature_selection import VarianceThreshold
 
 
 class RandomSelection(BaseEstimator, TransformerMixin):
@@ -28,4 +29,15 @@ class RandomSelection(BaseEstimator, TransformerMixin):
         n_samples, n_features = X.shape
         X_new = X[:, self.components]
 
+        return X_new
+
+class VarianceSelection():
+    """"Select best features based on their variance"""
+    def __init__(self, threshold=50):
+        self.threshold = threshold
+
+    def fit_transform(self, X, y=none):
+        X = check_array(X)
+        selection = VarianceThreshold(self.threshold)
+        X_new = selection.fit_transform(X)
         return X_new
