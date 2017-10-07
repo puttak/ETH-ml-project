7baf50bbf0ae39d87807bcdfae571bfcda4eba3b
Massimo Bortone <bortonem@student.ethz.ch>
2017-10-07 20:53:13 +0200

    added kbest selection class

--- ml_project/models/feature_selection.py
+++ ml_project/models/feature_selection.py
@@ -2,7 +2,7 @@ from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.utils import check_random_state
 from sklearn.utils.validation import check_array, check_is_fitted
 from sklearn.utils.random import sample_without_replacement
-from sklearn.feature_selection import VarianceThreshold
+from sklearn.feature_selection import VarianceThreshold, SelectKBest, f_regression
 
 
 class RandomSelection(BaseEstimator, TransformerMixin):
@@ -41,7 +41,25 @@ class VarianceSelection(BaseEstimator, TransformerMixin):
         X = check_array(X)
         self.components = VarianceThreshold(self.threshold)
         self.components.fit(X)
-        print(len(self.components.get_support(indices=True)))
+        return self
+
+    def transform(self, X, y=None):
+        check_is_fitted(self, ["components"])
+        X = check_array(X)
+        X_new = self.components.transform(X)
+
+        return X_new
+
+class KBestSelection(BaseEstimator, TransformerMixin):
+    """"Select best features based on their variance"""
+    def __init__(self, k):
+        self.k = k
+        self.components = None
+
+    def fit(self, X, y=None):
+        X = check_array(X)
+        self.components = SelectKBest(f_regression, k=self.k)
+        self.components.fit(X, y)
         return self
 
     def transform(self, X, y=None):
