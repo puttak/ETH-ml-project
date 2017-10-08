ba1d135d2924c24247cc4c679a1cfa04691f5d54
Massimo Bortone <bortonem@student.ethz.ch>
2017-10-08 13:11:51 +0200

    added standardization class to preprocessing module

--- ml_project/models/preprocessing.py
+++ ml_project/models/preprocessing.py
@@ -1,5 +1,19 @@
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.utils.validation import check_array
+from sklearn import preprocessing
+
+class Standardize(BaseEstimator, TransformerMixin):
+    """Rescale data so that features have the properties of a normal distribution"""
+    def __init__(self):
+        pass
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        X = check_array(X)
+        X_scaled = preprocessing.scale(X)
+        return X_scaled.reshape(X.shape[0], -1)
 
 
 class Flatten(BaseEstimator, TransformerMixin):
@@ -14,4 +28,4 @@ class Flatten(BaseEstimator, TransformerMixin):
         X = check_array(X)
         X = X.reshape(-1, 176, 208, 176) # Bad practice: hard-coded dimensions
         X = X.mean(axis=self.dim)
-        return X.reshape(X.shape[0], -1)
+        return X_scaled.reshape(X.shape[0], -1)
