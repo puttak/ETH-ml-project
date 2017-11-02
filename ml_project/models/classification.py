14222b4e47c172bd9b5f2fe0c8fad10f8c989102
Massimo Bortone <bortonem@student.ethz.ch>
2017-11-02 16:27:33 +0100

    added Spearman score function to MeanPredictor

--- ml_project/models/classification.py
+++ ml_project/models/classification.py
@@ -1,6 +1,7 @@
 import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.utils.validation import check_array, check_is_fitted
+from scipy.stats import spearmanr
 
 
 class MeanPredictor(BaseEstimator, TransformerMixin):
@@ -14,3 +15,11 @@ class MeanPredictor(BaseEstimator, TransformerMixin):
         check_is_fitted(self, ["mean"])
         n_samples, _ = X.shape
         return np.tile(self.mean, (n_samples, 1))
+
+    def score(self, X, y):
+        a = self.predict_proba(X)
+        rhos = np.zeros(a.shape[0])
+        for i in range(a.shape[0]):
+            rhos[i] = spearmanr(a[i, :], y[i, :], axis=0)[0]
+        score = rhos.mean()
+        return score
