35d204368e49b6177e2ffb2cf84d7e73ea6889d0
Massimo Bortone <bortonem@student.ethz.ch>
2017-12-03 22:52:27 +0100

    cleaned up code for 3rd project

--- ml_project/models/feature_selection.py
+++ ml_project/models/feature_selection.py
@@ -1,11 +1,10 @@
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.utils import check_random_state
-from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
+from sklearn.utils.validation import check_array, check_is_fitted
 from sklearn.utils.random import sample_without_replacement
 from sklearn.feature_selection import VarianceThreshold
-from sklearn.feature_selection import f_regression, f_classif
+from sklearn.feature_selection import f_regression
 from sklearn.feature_selection import SelectKBest, SelectPercentile
-from ml_project.models.utils import KMeansTransform
 
 
 class NonZeroSelection(BaseEstimator, TransformerMixin):
@@ -112,30 +111,6 @@ class PercentileSelection(BaseEstimator, TransformerMixin):
         check_is_fitted(self, ["components"])
         X = check_array(X)
         X_new = self.components.transform(X)
-        print("Number of features after PercentileSelection(percentile={}): {}"
-              .format(self.percentile, X_new.shape[1]))
-        return X_new
-
-
-class MLPercentileSelection(BaseEstimator, TransformerMixin):
-    """"Select best features based on their variance"""
-    def __init__(self, percentile=10, n_clusters=6):
-        self.percentile = percentile
-        self.n_clusters = n_clusters
-        self.mask = None
-
-    def fit(self, X, y):
-        X, y = check_X_y(X, y, multi_output=True)
-        y_new, clusters_proba = KMeansTransform(y, self.n_clusters)
-        selector = SelectPercentile(f_classif, percentile=self.percentile)
-        selector.fit(X, y_new)
-        self.mask = selector.get_support()
-        return self
-
-    def transform(self, X, y=None):
-        check_is_fitted(self, ["mask"])
-        X = check_array(X)
-        X_new = X[:, self.mask]
         print("Number of features after PercentileSelection(percentile={}): {}"
               .format(self.percentile, X_new.shape[1]))
         return X_new
