cfe0851a88774479e57f27fe9760d5321755c1fc
Massimo Bortone <bortonem@student.ethz.ch>
2017-12-10 23:50:30 +0100

    added random forest classifier

--- ml_project/models/classification.py
+++ ml_project/models/classification.py
@@ -1,93 +1,31 @@
 import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
-from sklearn.linear_model import LinearRegression, LogisticRegression
-from scipy.stats import spearmanr
-from ml_project.models.utils import KMeansTransform
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import f1_score
 
 
-class MeanPredictor(BaseEstimator, TransformerMixin):
-    """docstring for MeanPredictor"""
-    def fit(self, X, y):
-        self.mean = y.mean(axis=0)
-        return self
-
-    def predict_proba(self, X):
-        check_array(X)
-        check_is_fitted(self, ["mean"])
-        n_samples, _ = X.shape
-        return np.tile(self.mean, (n_samples, 1))
-
-    def score(self, X, y):
-        a = self.predict_proba(X)
-        rhos = np.zeros(a.shape[0])
-        for i in range(a.shape[0]):
-            rhos[i] = spearmanr(a[i, :], y[i, :], axis=0)[0]
-        score = rhos.mean()
-        return score
-
-
-class MLLinearPredictor(LinearRegression, TransformerMixin):
+class RandomForestPredictor(RandomForestClassifier, TransformerMixin):
     """
-    Perform linear regression on the data for every target label independently
+    Classification with randomized decision trees
     """
-    def __init__(self):
-        super(MLLinearPredictor, self).__init__()
+    def __init__(self, n_estimators=20, n_jobs=4,
+                 class_weight='balanced_subsample'):
+        super(RandomForestPredictor, self).__init__(
+            n_estimators=n_estimators, n_jobs=n_jobs, class_weight=class_weight)
 
     def fit(self, X, y):
         X, y = check_X_y(X, y, multi_output=True)
-        y = np.tan((y - 0.5)*np.pi)
-        super(MLLinearPredictor, self).fit(X, y)
+        super(RandomForestPredictor, self).fit(X, y)
         return self
 
-    def predict_proba(self, X):
-        check_is_fitted(self, ["coef_", "intercept_"])
+    def predict(self, X):
+        check_is_fitted(self, ["estimators_"])
         X = check_array(X)
-        prediction = super(MLLinearPredictor, self).predict(X)
-        prediction = np.arctan(prediction)*1.0/np.pi + 0.5
-        for i in range(prediction.shape[0]):
-            prediction[i, :] = prediction[i, :] / sum(prediction[i, :])
+        prediction = super(RandomForestPredictor, self).predict(X)
         return prediction
 
     def score(self, X, y):
-        a = self.predict_proba(X)
-        rhos = np.zeros(a.shape[0])
-        for i in range(a.shape[0]):
-            rhos[i] = spearmanr(a[i, :], y[i, :], axis=0)[0]
-        score = rhos.mean()
-        return score
-
-
-class MLLogisticPredictor(LogisticRegression, TransformerMixin):
-    """
-    Perform multinomial logistic regression on dataset
-    """
-    def __init__(self, n_clusters=6, C=1.0, solver='sag'):
-        self.n_clusters = n_clusters
-        self.clusters_proba = []
-        super(MLLogisticPredictor, self).__init__(
-            C=C, solver=solver, multi_class='multinomial',
-            class_weight='balanced', n_jobs=n_clusters)
-
-    def fit(self, X, y):
-        X, y = check_X_y(X, y, multi_output=True)
-        y_new, self.clusters_proba = KMeansTransform(y, self.n_clusters)
-        super(MLLogisticPredictor, self).fit(X, y_new)
-        return self
-
-    def predict_proba(self, X):
-        check_is_fitted(self, ["coef_", "intercept_"])
-        X = check_array(X)
-        prediction = super(MLLogisticPredictor, self).predict(X)
-        class_proba = np.zeros((X.shape[0], 4))
-        for i in range(X.shape[0]):
-            class_proba[i] = self.clusters_proba[prediction[i]]
-        return class_proba
-
-    def score(self, X, y):
-        a = self.predict_proba(X)
-        rhos = np.zeros(a.shape[0])
-        for i in range(a.shape[0]):
-            rhos[i] = spearmanr(a[i, :], y[i, :], axis=0)[0]
-        score = rhos.mean()
+        prediction = self.predict(X)
+        score = f1_score(prediction, y, average='micro')
         return score
