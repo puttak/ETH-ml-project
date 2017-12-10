554204697e7cde1f77223a0d7c2530880d402784
Massimo Bortone <bortonem@student.ethz.ch>
2017-12-11 00:23:27 +0100

    added debugging text

--- ml_project/models/classification.py
+++ ml_project/models/classification.py
@@ -10,22 +10,30 @@ class RandomForestPredictor(RandomForestClassifier, TransformerMixin):
     Classification with randomized decision trees
     """
     def __init__(self, n_estimators=20, n_jobs=4,
-                 class_weight='balanced_subsample'):
+                 class_weight='balanced_subsample', debug=False):
         super(RandomForestPredictor, self).__init__(
             n_estimators=n_estimators, n_jobs=n_jobs, class_weight=class_weight)
+        self.debug = debug
 
     def fit(self, X, y):
         X, y = check_X_y(X, y, multi_output=True)
         super(RandomForestPredictor, self).fit(X, y)
+        if self.debug:
+            print("Fitting {} samples:".format(X.shape[0]))
+            print("\t- feature importance: {}".format(self.feature_importances_))
         return self
 
     def predict(self, X):
         check_is_fitted(self, ["estimators_"])
         X = check_array(X)
         prediction = super(RandomForestPredictor, self).predict(X)
-        return prediction
+        if self.debug:
+            print("Predicting {} samples".format(X.shape[0]))
+        return int(prediction)
 
     def score(self, X, y):
         prediction = self.predict(X)
         score = f1_score(prediction, y, average='micro')
+        if self.debug:
+            print("Score for {} samples: {}".format(X.shape[0], score))
         return score
